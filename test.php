e866bc79d6d187586783e914a800aa54c3a95743
Debian <bitnami@ip-172-26-0-149.eu-west-3.compute.internal>
2023-11-23 10:13:09 +0000

    uodates

--- test.php
+++ test.php
@@ -10,9 +10,9 @@ $request = file_get_contents("php://input");
 
 $data = json_decode($request, true);
 
-/**  
- * set your custom session id and start session for the incoming request.  
- * Note!! 
+ 
+/**session id and start session for the incoming request.  
+  Note!! 
  * The only unique parameter is the msisdn.   
  * Set session id with the msisdn in order to track the session  
  */
@@ -21,8 +21,6 @@ $data = json_decode($request, true);
 
 session_id(md5($data['MSISDN']));
 session_start();
-
-
 
 
 $ussd_id = $data['USERID'];
@@ -42,30 +40,7 @@ function sendResponse($responsePayload)
     }
 }
 
-function logJSONData($APIres, $pathToLogFile)
-{
-    //TODO: READ FROM THE JSON FILE
-    $sample_json = file_get_contents($pathToLogFile);
 
-    //EDGE-CASE:IF THE JSON FILE IS EMPTY
-    if (strlen($sample_json) <= 2) {
-        //TODO: CREATE SOME EMPTY ARRAY & POPULATE IT
-        $sample_arr = [];
-        $sample_json1 = json_decode($APIres, true);
-        array_push($sample_arr, $sample_json1);
-
-        //TODO:PARSE THE POPULATED DATA INTO JSON & WRITE IT INTO THE JSON FILE
-        file_put_contents($pathToLogFile, json_encode($sample_arr));
-    } else {
-
-        $decodedJSONArr = json_decode($sample_json, true);
-
-        array_push($decodedJSONArr, json_decode($APIres));
-
-        file_put_contents($pathToLogFile, json_encode($decodedJSONArr));
-    }
-};
-// logJSONData($request,'./logs.json');
 
 
 
@@ -74,23 +49,21 @@ if (isset($_SESSION[$id]) and $msgtype == false) {
 
     $_SESSION[$id] = "{$_SESSION[$id]}{$user_data},"; //"scrn1 Option" -> "scrn1Option,scrn2Option"
 
-    //TODO: Validate the input
+    // Validate the input
 
     if (!preg_match("/^[1-3,]+$/", $_SESSION[$id])) {
-        $msg = "Invalid Response buddy, redial the code and try again :)";
+        $msg = "Invalid Response,oops , redial the code and try again :)";
         $resp = array("USERID" => $ussd_id, "MSISDN" => $msisdn, "USERDATA" => $user_data, "MSG" => $msg, "MSGTYPE" => false);
         echo json_encode($resp);
         session_destroy();
         die();
     }
-    file_put_contents('./res.txt', "{$_SESSION[$id]}\n", FILE_APPEND);
+    
 
+//Split the string of selected options by ","
+    $selectedOptions = preg_split("/\,/", $_SESSION[$id]); //["screen 1 option ","scrn 2 Option"]
 
-    //TODO: Split the string of selected options by ","
-    $selectedOptions = preg_split("/\,/", $_SESSION[$id]); //["scrn1Option","scrn2Option"]
-
-    file_put_contents('./res.txt', "{$selectedOptions}\n", FILE_APPEND);
-
+    
     $moods = [
         "1" => "not fine",
         "2" => "feeling frisky",
@@ -113,7 +86,7 @@ if (isset($_SESSION[$id]) and $msgtype == false) {
 
     if (count($selectedOptions) >= $totalScreens) {
 
-        $msg = "Tough life bro, you are {$moods[$moodKey]} because of {$moodCauses[$moodCauseKey]} issues :(";
+        $msg = "Hmm i think , you are {$moods[$moodKey]} because of {$moodCauses[$moodCauseKey]} issues :(";
         $resp = array("USERID" => $ussd_id, "MSISDN" => $msisdn, "USERDATA" => $user_data, "MSG" => $msg, "MSGTYPE" => false);
         sendResponse($resp);
     } else {
@@ -137,7 +110,7 @@ else {
 
 
     /**
-     * Stores user inputs using sessions. 
+     *soring user's  inputs using sessions. 
      * You may also store user inputs in a database
      */
 
